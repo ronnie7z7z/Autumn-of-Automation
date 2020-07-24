8444bd53126fecfd4d4603dcb24aef187183d947
ronnie7z7z <ronitrshukla37@gmai.com>
2020-07-24 23:49:39 +0530

    A2-2

--- OpenCV/test.py
+++ OpenCV/test.py
@@ -191,16 +191,66 @@
 
 #CONTOURS
 
+# import numpy as np
+# import cv2
+# from matplotlib import pyplot as plt
+
+# im = cv2.imread('dp2.jpg')
+# imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+# ret,thresh = cv2.threshold(imgray,127,255,0)
+# contours, h = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+# img = cv2.drawContours(imgray, contours, -1, (0,0,0), 3)
+# plt.imshow(img, 'gray')
+# plt.show()
+# cv2.destroyAllWindows()
+
+#HAAR CASCADES FACE AND EYE DETECTION
+#PIC DETECTION
+
+# import numpy as np
+# import cv2
+
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# img = cv2.imread('dp2.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+# for (x,y,w,h) in faces:
+#     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+#     roi_gray = gray[y:y+h, x:x+w]
+#     roi_color = img[y:y+h, x:x+w]
+#     eyes = eye_cascade.detectMultiScale(roi_gray)
+#     for (ex,ey,ew,eh) in eyes:
+#         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+# cv2.imshow('img',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#LIVE DETECTION
+
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-im = cv2.imread('dp2.jpg')
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
-contours, h = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-img = cv2.drawContours(imgray, contours, -1, (0,0,0), 3)
-plt.imshow(img, 'gray')
-plt.show()
-cv2.destroyAllWindows()
+capture = cv2.VideoCapture(0)
+while(True):
+     
+    ret, img = capture.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('video', img)
+    if cv2.waitKey(1) == 27:
+        break
+capture.release()
